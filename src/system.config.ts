c8297df916c9c46fee43cb848ebfb183c910064f
Shyam Chen <shyamchen1994@gmail.com>
2016-08-10 15:45:54 +0800

    4th Commit

--- src/system.config.ts
+++ src/system.config.ts
@@ -1,15 +1,22 @@
 const ngVer = '@2.0.0-rc.4';
 
-const  map = {
-  'app':                        'app',
-  'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+const map = {
+  'app': 'app',
+  'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
   'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api'
 };
 
 const packages = {
-  'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
-  'rxjs':                       { defaultExtension: 'js' },
-  'angular2-in-memory-web-api': { defaultExtension: 'js' },
+  'app': {
+    main: 'main.ts',
+    defaultExtension: 'ts'
+  },
+  'rxjs': {
+    defaultExtension: 'js'
+  },
+  'angular2-in-memory-web-api': {
+    defaultExtension: 'js'
+  }
 };
 
 const packageNames = [
@@ -17,18 +24,20 @@ const packageNames = [
   '@angular/compiler',
   '@angular/core',
   '@angular/http',
-  '@angular/router',
   '@angular/platform-browser',
   '@angular/platform-browser-dynamic',
   '@angular/upgrade'
 ];
 
-packageNames.forEach(function(pkgName) {
-  map[pkgName] = 'https://npmcdn.com/' + pkgName + ngVer;
+packageNames.forEach((pkgName: any) => {
+  map[pkgName] = `https://npmcdn.com/${pkgName}${ngVer}`;
 });
 
-packageNames.forEach(function(pkgName) {
-  packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+packageNames.forEach((pkgName: any) => {
+  packages[pkgName] = {
+    main: 'index.js',
+    defaultExtension: 'js'
+  };
 });
 
 const config = {
@@ -38,4 +47,4 @@ const config = {
   },
   map: map,
   packages: packages
-}
+};
